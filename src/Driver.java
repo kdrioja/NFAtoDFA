6e13af982a3d496cda828ebd986cc4aded6cc372
Kenia Rioja <kdrioja@gmail.com>
2018-10-16 16:57:36 -0700

    Finished test cases.

--- src/Driver.java
+++ src/Driver.java
@@ -82,7 +82,84 @@ public class Driver {
         System.out.println(b.transformToDFA());
     }
 
+    public static void testCaseC() {
+        //c
+        ArrayList<String> inputAlphC = new ArrayList<>();
+        inputAlphC.add("a");
+        inputAlphC.add("b");
+        inputAlphC.add("L");
+        LinkedHashSet<Integer> finalVars = new LinkedHashSet<>();
+        finalVars.add(1);
+        NFA c = new NFA(0, finalVars, inputAlphC, true);
+
+        ArrayList<Pair<String, LinkedHashSet<Integer>>> tempArray = new ArrayList<>();
+        LinkedHashSet<Integer> tempSet = new LinkedHashSet<>();
+        tempSet.add(0);
+        tempSet.add(1);
+        tempArray.add(new Pair<>("a", tempSet));
+        tempArray.add(new Pair<>("b", null));
+        tempArray.add(new Pair<>("L", null));
+        c.addState(new State(0, false, tempArray));
+
+
+        tempArray = new ArrayList<>();
+        tempSet = new LinkedHashSet<>();
+        tempSet.add(1);
+        tempSet.add(2);
+        tempArray.add(new Pair<>("a", null));
+        tempArray.add(new Pair<>("b", tempSet));
+        tempArray.add(new Pair<>("L", tempSet));
+        c.addState(new State(1, true, tempArray));
+
+        tempArray = new ArrayList<>();
+        tempSet = new LinkedHashSet<>();
+        tempSet.add(2);
+        tempArray.add(new Pair<>("a", tempSet));
+        tempArray.add(new Pair<>("b", null));
+        tempArray.add(new Pair<>("L", null));
+        c.addState(new State(2, false, tempArray));
+
+        System.out.println(c);
+        System.out.println(c.transformToDFA());
+    }
+
+    public static void testCaseD() {
+        //d
+        ArrayList<String> inputAlphA = new ArrayList<>();
+        inputAlphA.add("a");
+        inputAlphA.add("b");
+        LinkedHashSet<Integer> finalVars = new LinkedHashSet<>();
+        finalVars.add(1);
+        NFA d = new NFA(0, finalVars, inputAlphA, false);
+
+        ArrayList<Pair<String, LinkedHashSet<Integer>>> tempArray = new ArrayList<>();
+        LinkedHashSet<Integer> tempSet = new LinkedHashSet<>();
+        tempSet.add(0);
+        tempSet.add(1);
+        tempArray.add(new Pair<>("a", tempSet));
+        tempArray.add(new Pair<>("b", null));
+        d.addState(new State(0, false, tempArray));
+
+
+        tempArray = new ArrayList<>();
+        tempSet = new LinkedHashSet<>();
+        tempSet.add(2);
+        tempArray.add(new Pair<>("a", tempSet));
+        tempArray.add(new Pair<>("b", tempSet));
+        d.addState(new State(1, true, tempArray));
+
+        tempArray = new ArrayList<>();
+        tempSet = new LinkedHashSet<>();
+        tempSet.add(2);
+        tempArray.add(new Pair<>("a", null));
+        tempArray.add(new Pair<>("b", tempSet));
+        d.addState(new State(2, false, tempArray));
+
+        System.out.println(d);
+        System.out.println(d.transformToDFA());
+    }
+
     public static void main(String[] args) {
-        testCaseB();
+        testCaseD();
     }
 }
